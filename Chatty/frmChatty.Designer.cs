7366944775240f2fb8bf0dde7b379319336b3217
Steven <stevenmhutchinson@hotmail.com>
2019-04-06 18:49:00 -0400

    Added UI controls.

--- Chatty/frmChatty.Designer.cs
+++ Chatty/frmChatty.Designer.cs
@@ -28,13 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbClientMessages = new System.Windows.Forms.GroupBox();
+            this.rtbClientMessages = new System.Windows.Forms.RichTextBox();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.HomePage = new System.Windows.Forms.TabPage();
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.clbActiveClients = new System.Windows.Forms.CheckedListBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.TbMsg = new System.Windows.Forms.TextBox();
+            this.SettingsPage = new System.Windows.Forms.TabPage();
+            this.gbClientMessages.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.HomePage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbClientMessages
+            // 
+            this.gbClientMessages.Controls.Add(this.rtbClientMessages);
+            this.gbClientMessages.Location = new System.Drawing.Point(8, 6);
+            this.gbClientMessages.Name = "gbClientMessages";
+            this.gbClientMessages.Size = new System.Drawing.Size(660, 356);
+            this.gbClientMessages.TabIndex = 4;
+            this.gbClientMessages.TabStop = false;
+            this.gbClientMessages.Text = "Chatty Console";
+            // 
+            // rtbClientMessages
+            // 
+            this.rtbClientMessages.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbClientMessages.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.rtbClientMessages.Location = new System.Drawing.Point(6, 19);
+            this.rtbClientMessages.Name = "rtbClientMessages";
+            this.rtbClientMessages.ReadOnly = true;
+            this.rtbClientMessages.Size = new System.Drawing.Size(648, 331);
+            this.rtbClientMessages.TabIndex = 3;
+            this.rtbClientMessages.Text = "";
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.HomePage);
+            this.tabControl.Controls.Add(this.SettingsPage);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(804, 451);
+            this.tabControl.TabIndex = 2;
+            // 
+            // HomePage
+            // 
+            this.HomePage.BackColor = System.Drawing.SystemColors.Window;
+            this.HomePage.Controls.Add(this.lbUsername);
+            this.HomePage.Controls.Add(this.clbActiveClients);
+            this.HomePage.Controls.Add(this.btnConnect);
+            this.HomePage.Controls.Add(this.tbUsername);
+            this.HomePage.Controls.Add(this.gbClientMessages);
+            this.HomePage.Controls.Add(this.btnSend);
+            this.HomePage.Controls.Add(this.TbMsg);
+            this.HomePage.Location = new System.Drawing.Point(4, 22);
+            this.HomePage.Name = "HomePage";
+            this.HomePage.Padding = new System.Windows.Forms.Padding(3);
+            this.HomePage.Size = new System.Drawing.Size(796, 425);
+            this.HomePage.TabIndex = 0;
+            this.HomePage.Text = "Chatty";
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Location = new System.Drawing.Point(701, 10);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(58, 13);
+            this.lbUsername.TabIndex = 8;
+            this.lbUsername.Text = "Username:";
+            // 
+            // clbActiveClients
+            // 
+            this.clbActiveClients.FormattingEnabled = true;
+            this.clbActiveClients.Location = new System.Drawing.Point(671, 85);
+            this.clbActiveClients.Name = "clbActiveClients";
+            this.clbActiveClients.Size = new System.Drawing.Size(119, 334);
+            this.clbActiveClients.TabIndex = 7;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(671, 52);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(119, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(671, 26);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(119, 20);
+            this.tbUsername.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(14, 394);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(648, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // TbMsg
+            // 
+            this.TbMsg.Location = new System.Drawing.Point(14, 368);
+            this.TbMsg.Name = "TbMsg";
+            this.TbMsg.Size = new System.Drawing.Size(648, 20);
+            this.TbMsg.TabIndex = 0;
+            // 
+            // SettingsPage
+            // 
+            this.SettingsPage.Location = new System.Drawing.Point(4, 22);
+            this.SettingsPage.Name = "SettingsPage";
+            this.SettingsPage.Padding = new System.Windows.Forms.Padding(3);
+            this.SettingsPage.Size = new System.Drawing.Size(796, 425);
+            this.SettingsPage.TabIndex = 1;
+            this.SettingsPage.Text = "Settings";
+            this.SettingsPage.UseVisualStyleBackColor = true;
+            // 
+            // frmChatty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(804, 451);
+            this.Controls.Add(this.tabControl);
+            this.Name = "frmChatty";
+            this.Text = "Chatty";
+            this.gbClientMessages.ResumeLayout(false);
+            this.tabControl.ResumeLayout(false);
+            this.HomePage.ResumeLayout(false);
+            this.HomePage.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbClientMessages;
+        private System.Windows.Forms.RichTextBox rtbClientMessages;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage HomePage;
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.CheckedListBox clbActiveClients;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox TbMsg;
+        private System.Windows.Forms.TabPage SettingsPage;
     }
 }
 
--- ChattyServer/frmServer.Designer.cs
+++ ChattyServer/frmServer.Designer.cs
@@ -28,13 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.SettingsPage = new System.Windows.Forms.TabPage();
+            this.btnStopListening = new System.Windows.Forms.Button();
+            this.gbConnectedClients = new System.Windows.Forms.GroupBox();
+            this.clbConnectedClients = new System.Windows.Forms.CheckedListBox();
+            this.StatusPage = new System.Windows.Forms.TabPage();
+            this.gbServerMessages = new System.Windows.Forms.GroupBox();
+            this.rtbServerMessages = new System.Windows.Forms.RichTextBox();
+            this.cbSendToAll = new System.Windows.Forms.CheckBox();
+            this.nudSpamAmount = new System.Windows.Forms.NumericUpDown();
+            this.btnSpam = new System.Windows.Forms.Button();
+            this.tbMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tsslReceivedMessages = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslServerStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.SettingsPage.SuspendLayout();
+            this.gbConnectedClients.SuspendLayout();
+            this.StatusPage.SuspendLayout();
+            this.gbServerMessages.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSpamAmount)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(6, 6);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(95, 23);
+            this.btnListen.TabIndex = 0;
+            this.btnListen.Text = "Start Server";
+            this.btnListen.UseVisualStyleBackColor = true;
+            // 
+            // SettingsPage
+            // 
+            this.SettingsPage.BackColor = System.Drawing.SystemColors.Window;
+            this.SettingsPage.Controls.Add(this.btnStopListening);
+            this.SettingsPage.Controls.Add(this.btnListen);
+            this.SettingsPage.Location = new System.Drawing.Point(4, 22);
+            this.SettingsPage.Name = "SettingsPage";
+            this.SettingsPage.Padding = new System.Windows.Forms.Padding(3);
+            this.SettingsPage.Size = new System.Drawing.Size(796, 535);
+            this.SettingsPage.TabIndex = 1;
+            this.SettingsPage.Text = "Settings";
+            // 
+            // btnStopListening
+            // 
+            this.btnStopListening.Location = new System.Drawing.Point(6, 35);
+            this.btnStopListening.Name = "btnStopListening";
+            this.btnStopListening.Size = new System.Drawing.Size(95, 23);
+            this.btnStopListening.TabIndex = 1;
+            this.btnStopListening.Text = "Stop Server";
+            this.btnStopListening.UseVisualStyleBackColor = true;
+            // 
+            // gbConnectedClients
+            // 
+            this.gbConnectedClients.Controls.Add(this.clbConnectedClients);
+            this.gbConnectedClients.Location = new System.Drawing.Point(600, 6);
+            this.gbConnectedClients.Name = "gbConnectedClients";
+            this.gbConnectedClients.Size = new System.Drawing.Size(193, 439);
+            this.gbConnectedClients.TabIndex = 14;
+            this.gbConnectedClients.TabStop = false;
+            this.gbConnectedClients.Text = "Connected Clients";
+            // 
+            // clbConnectedClients
+            // 
+            this.clbConnectedClients.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Bold);
+            this.clbConnectedClients.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.clbConnectedClients.FormattingEnabled = true;
+            this.clbConnectedClients.Location = new System.Drawing.Point(6, 19);
+            this.clbConnectedClients.Name = "clbConnectedClients";
+            this.clbConnectedClients.Size = new System.Drawing.Size(181, 400);
+            this.clbConnectedClients.TabIndex = 11;
+            // 
+            // StatusPage
+            // 
+            this.StatusPage.BackColor = System.Drawing.SystemColors.Window;
+            this.StatusPage.Controls.Add(this.gbConnectedClients);
+            this.StatusPage.Controls.Add(this.gbServerMessages);
+            this.StatusPage.Controls.Add(this.cbSendToAll);
+            this.StatusPage.Controls.Add(this.nudSpamAmount);
+            this.StatusPage.Controls.Add(this.btnSpam);
+            this.StatusPage.Controls.Add(this.tbMsg);
+            this.StatusPage.Controls.Add(this.btnSend);
+            this.StatusPage.Location = new System.Drawing.Point(4, 22);
+            this.StatusPage.Name = "StatusPage";
+            this.StatusPage.Padding = new System.Windows.Forms.Padding(3);
+            this.StatusPage.Size = new System.Drawing.Size(796, 535);
+            this.StatusPage.TabIndex = 0;
+            this.StatusPage.Text = "Status";
+            // 
+            // gbServerMessages
+            // 
+            this.gbServerMessages.Controls.Add(this.rtbServerMessages);
+            this.gbServerMessages.Location = new System.Drawing.Point(6, 6);
+            this.gbServerMessages.Name = "gbServerMessages";
+            this.gbServerMessages.Size = new System.Drawing.Size(588, 439);
+            this.gbServerMessages.TabIndex = 13;
+            this.gbServerMessages.TabStop = false;
+            this.gbServerMessages.Text = "Server Console";
+            // 
+            // rtbServerMessages
+            // 
+            this.rtbServerMessages.Font = new System.Drawing.Font("Rubik", 9.75F, System.Drawing.FontStyle.Bold);
+            this.rtbServerMessages.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.rtbServerMessages.Location = new System.Drawing.Point(6, 19);
+            this.rtbServerMessages.Name = "rtbServerMessages";
+            this.rtbServerMessages.ReadOnly = true;
+            this.rtbServerMessages.Size = new System.Drawing.Size(576, 414);
+            this.rtbServerMessages.TabIndex = 12;
+            this.rtbServerMessages.Text = "";
+            // 
+            // cbSendToAll
+            // 
+            this.cbSendToAll.AutoSize = true;
+            this.cbSendToAll.Location = new System.Drawing.Point(6, 460);
+            this.cbSendToAll.Name = "cbSendToAll";
+            this.cbSendToAll.Size = new System.Drawing.Size(81, 17);
+            this.cbSendToAll.TabIndex = 10;
+            this.cbSendToAll.Text = "Send To All";
+            this.cbSendToAll.UseVisualStyleBackColor = true;
+            // 
+            // nudSpamAmount
+            // 
+            this.nudSpamAmount.Location = new System.Drawing.Point(600, 459);
+            this.nudSpamAmount.Name = "nudSpamAmount";
+            this.nudSpamAmount.Size = new System.Drawing.Size(67, 20);
+            this.nudSpamAmount.TabIndex = 8;
+            // 
+            // btnSpam
+            // 
+            this.btnSpam.Location = new System.Drawing.Point(673, 458);
+            this.btnSpam.Name = "btnSpam";
+            this.btnSpam.Size = new System.Drawing.Size(114, 23);
+            this.btnSpam.TabIndex = 7;
+            this.btnSpam.Text = "Spam";
+            this.btnSpam.UseVisualStyleBackColor = true;
+            // 
+            // tbMsg
+            // 
+            this.tbMsg.Location = new System.Drawing.Point(12, 487);
+            this.tbMsg.Name = "tbMsg";
+            this.tbMsg.Size = new System.Drawing.Size(775, 20);
+            this.tbMsg.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(94, 458);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(500, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // tsslReceivedMessages
+            // 
+            this.tsslReceivedMessages.Name = "tsslReceivedMessages";
+            this.tsslReceivedMessages.Size = new System.Drawing.Size(120, 17);
+            this.tsslReceivedMessages.Text = "Received Messages: 0";
+            // 
+            // tsslServerStatus
+            // 
+            this.tsslServerStatus.Name = "tsslServerStatus";
+            this.tsslServerStatus.Size = new System.Drawing.Size(26, 17);
+            this.tsslServerStatus.Text = "Idle";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslServerStatus,
+            this.tsslReceivedMessages});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(804, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.StatusPage);
+            this.tabControl1.Controls.Add(this.SettingsPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(804, 561);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // frmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(804, 561);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "frmServer";
+            this.Text = "Server";
+            this.SettingsPage.ResumeLayout(false);
+            this.gbConnectedClients.ResumeLayout(false);
+            this.StatusPage.ResumeLayout(false);
+            this.StatusPage.PerformLayout();
+            this.gbServerMessages.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSpamAmount)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.TabPage SettingsPage;
+        private System.Windows.Forms.Button btnStopListening;
+        private System.Windows.Forms.GroupBox gbConnectedClients;
+        private System.Windows.Forms.CheckedListBox clbConnectedClients;
+        private System.Windows.Forms.TabPage StatusPage;
+        private System.Windows.Forms.GroupBox gbServerMessages;
+        private System.Windows.Forms.RichTextBox rtbServerMessages;
+        private System.Windows.Forms.CheckBox cbSendToAll;
+        private System.Windows.Forms.NumericUpDown nudSpamAmount;
+        private System.Windows.Forms.Button btnSpam;
+        private System.Windows.Forms.TextBox tbMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ToolStripStatusLabel tsslReceivedMessages;
+        private System.Windows.Forms.ToolStripStatusLabel tsslServerStatus;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
 
